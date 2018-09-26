62637c84c9188864f1d42a5d3a6ab0b113307966
xr <xiaorui2017@gmail.com>
2018-09-26 10:04:23 +0800

    在xml属性中嵌入scala代码

--- src/main/scala/cn/scala/study/xml/xmlForScala.scala
+++ src/main/scala/cn/scala/study/xml/xmlForScala.scala
@@ -1,13 +1,35 @@
 package scala.study.xml
 
 class xmlForScala {
-  //scala代码插入xml
-val books =Array("book1","book2")
- val booksXml = <books><book>{books(0)}</book><book>{books(1)}</book></books>
+  //将scala代码插入到xml
+  val books = Array("book1", "book2")
+  val booksXml = <books>
+    <book>
+      {books(0)}
+    </book> <book>
+      {books(1)}
+    </book>
+  </books>
 
   booksXml.label
   booksXml.child
 
-  //通过xml动态生成scala代码
 
+  <books>
+    {for (book <- books) yield <book>
+    {book}
+  </book>}
+  </books>
+
+    //在xml属性中嵌入scala代码
+    <book id="{books(0)}">
+      {books(0)}
+    </book>
+
+  val bookIds = Map("book1" -> 1, "book2" -> 2)
+  <books>
+    {for ((name, id) <- bookIds) yield <book id={id}>
+    {name}
+  </book>}
+  </books>
 }
